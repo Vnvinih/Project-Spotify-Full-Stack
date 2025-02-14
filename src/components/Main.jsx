df349fab89f3836eb00c4f0128e225cfc687d636
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-02-14 20:49:44 -0300

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,26 +3,37 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className="main">
+
       {/* Item list de Artistas */}
+      {type === "artists" || type === undefined ? ( 
       <ItemList 
-        title="Artistas" 
-        items={5} 
-        itemsArray={artistArray} 
-        path='/artists' 
-        idPath="/artist"
-        />
+          title="Artistas" 
+          items={10} 
+          itemsArray={artistArray} 
+          path='/artists' 
+          idPath="/artist"
+        /> 
+       ) : (
+         <></>
+      )}
+     
 
       {/* Item list de Música */}
-        <ItemList 
-        title="Músicas" 
-        items={10} 
-        itemsArray={songsArray} 
-        path='/songs' 
-        idPath="/songs"
-        />
+      {type ===  "songs" || type === undefined ?  (
+      <ItemList 
+          title="Músicas" 
+          items={20} 
+          itemsArray={songsArray} 
+          path='/songs' 
+          idPath="/songs"
+        /> 
+        ) : (
+          <></>
+       )}
+      
 
     </div>
   );
