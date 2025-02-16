626401f7d506c1be174ecacc18a5edef2baec716
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-02-16 15:59:20 -0300

    Update ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,27 +3,25 @@ import SingleItem from "./SingleItem";
 import { Link, useLocation } from "react-router-dom";
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
-  // console.log(items)
-  // console.log(useLocation())
-  const {pathname} =useLocation()
-  // console.log(pathname)
-
-  const isHome = pathname === '/'
-  const finalItems = isHome ? items :  Infinity
+  // console.log(items);
+  // console.log(useLocation());
+  const { pathname } = useLocation();
+  // console.log(pathname);
+  const isHome = pathname === "/";
+  const finalItems = isHome ? items : Infinity;
 
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        
+
         {isHome ? (
-          <Link to={path} className="item-list__link" >
+          <Link to={path} className="item-list__link">
             Mostrar tudo
           </Link>
-         ) : (
-        <></>
+        ) : (
+          <></>
         )}
-        
       </div>
 
       <div className="item-list__container">
@@ -31,13 +29,12 @@ const ItemList = ({ title, items, itemsArray, path, idPath }) => {
           .filter((currentValue, index) => index < finalItems)
           .map((currObj, index) => (
             <SingleItem
-            //  id={currObj.id}
-            //  name={currObj.name}
-            //  image={currObj.image}
-            //  banner={currObj.banner}
-            
-            idPath={idPath}
-            {...currObj}
+              // id={currObj.id}
+              // name={currObj.name}
+              // image={currObj.image}
+              // banner={currObj.banner}
+              {...currObj}
+              idPath={idPath}
               key={`${title}-${index}`}
             />
           ))}
