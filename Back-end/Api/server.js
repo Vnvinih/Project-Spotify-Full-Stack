a7982a8984708c008b10408c4f27b369bfe9ffc9
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-02-16 16:06:55 -0300

    Update server.js

--- Back-end/Api/server.js
+++ Back-end/Api/server.js
@@ -1,22 +1,31 @@
-import express from 'express'
-import {artistArray} from "../../src/assets/database/artists.js"
-import {songsArray} from "../../src/assets/database/songs.js"
+// API significa Application Programming Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create Read Update Delete
+// Endpoint
+// Middleware
 
-const app = express()
-const PORT = 3000
+import express from "express";
+import cors from "cors";
+import { db } from "./connect.js";
 
-app.get('/', (request, response) => {
-    response.send("Só vamos trabalhar comm os endpoints '/artists' e '/songs' ")
-})
+const app = express();
+const PORT = 3001;
 
-app.get('/artists', (request, response) => {
-    response.send(artistArray)
-})
+app.use(cors());
+// app.use(express.json());
 
-app.get('/songs', (request, response) => {
-    response.send(songsArray)
-})
+app.get("/", (request, response) => {
+  response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
+});
+
+app.get("/artists", async (request, response) => {
+  response.send(await db.collection("artists").find({}).toArray());
+});
+
+app.get("/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor está escutando na porta ${PORT}`)
-})
+  console.log(`Servidor está escutando na porta ${PORT}`);
+});
